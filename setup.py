2eb1b583078cf4e7a57d2ef216d9e610bb006f3c
umatbro <umatbroo@gmail.com>
2019-11-17 15:57:39 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
-from setuptools import setup, find_packages
-from os import path
-from codecs import open
-
-
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-setup(
-    name='um',
-    version='1.5.0',
-    description='Package with utilities',
-    long_description=long_description,  # read this form README.rst
-    url='https://github.com/umatbro/u-utils',
-    author='umat',
-    author_email='umatbroo@gmail.com',
-    license='MIT',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities',
-    ],
-    keywords='utils',
-    packages=find_packages(exclude=['docs', 'other']),
-    install_requires=[
-        'numpy==1.14.5',
-        'pandas==0.23.3',
-    ],
-    python_requires='>=3',
-    entry_points={
-        'console_scripts': []
-    }
-)
+# from setuptools import setup, find_packages
+# from os import path
+# from codecs import open
+#
+#
+# here = path.abspath(path.dirname(__file__))
+# with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#     long_description = f.read()
+#
+#
+# setup(
+#     name='um',
+#     version='1.5.0',
+#     description='Package with utilities',
+#     long_description=long_description,  # read this form README.rst
+#     url='https://github.com/umatbro/u-utils',
+#     author='umat',
+#     author_email='umatbroo@gmail.com',
+#     license='MIT',
+#     classifiers=[
+#         'License :: OSI Approved :: MIT License',
+#         'Intended Audience :: Developers',
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3.5',
+#         'Programming Language :: Python :: 3.6',
+#         'Topic :: Utilities',
+#     ],
+#     keywords='utils',
+#     packages=find_packages(exclude=['docs', 'other']),
+#     install_requires=[
+#         'numpy==1.14.5',
+#         'pandas==0.23.3',
+#     ],
+#     python_requires='>=3',
+#     entry_points={
+#         'console_scripts': []
+#     }
+# )
